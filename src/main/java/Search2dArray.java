80c52b2e69148c9d090647dea45c6f9605c67e99
Justin Byrd <officialjbyrd@gmail.com>
2023-03-08 04:40:18 +0000

    Done

--- src/main/java/Search2dArray.java
+++ src/main/java/Search2dArray.java
@@ -10,6 +10,14 @@ public class Search2dArray {
      * @return true if nums contains target, and false otherwise.
      */
     public boolean search(int[][] nums, int target){
-        return false;
+        boolean yep = false;
+        for (int i = 0; i < nums.length; i++){
+            for (int j = 0; j < nums.length; j++){
+                if (nums[i][j] == target){
+                    yep = true;
+                }
+            }
+        }
+        return yep;
     }
 }
